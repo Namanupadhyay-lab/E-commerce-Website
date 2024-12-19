547e5fa32fed58005179b83b47c87ce4cf3e8808
Namanupadhyay-lab <namanupadhyay631@gmail.com>
2024-12-20 01:49:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
-$(document).ready(function () {
-    $("a").on("click", function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-  
-        var hash = this.hash;
-        $("html, body").animate(
-          {
-            scrollTop: $(hash).offset().top,
-          },
-          800,
-          function () {
-            window.location.hash = hash;
-          }
-        );
-      }
-    });
-  });
-  
-  $(".menu-items a").click(function () {
-    $("#checkbox").prop("checked", false);
-  });
+const bar = document.getElementById('bar');
+const close = document.getElementById('close')
+const nav = document.getElementById('navbar');
 
-  function myFunction() {
-    var txt;
-    if (confirm("Confirm to add to cart!")) {
-      txt = "OK! Your product added to cart";
-    } else {
-      txt = "You Canceled the to add to cart";
-    }
-  }
-  
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close){
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+}
+
+const subBtn = document.getElementById("SubBtn")
+
+function subProm(){
+    window.alert("submitted sucessfully")
+}
+
+const siUP = document.getElementById("siUP")
+
+function signIN() {
+    window.alert("signed in sucessfully!!")
+}
+
+const sinIn = document.getElementById("sinIn")
+function sgn(){
+    let a = prompt("Email")
+    let b = prompt("Password")
+
+    window.alert(a + "\n" + "*******" + "\n" + "Signup sucessfully")
+}
+
+const item1 = document.querySelector("#item1")
+function rv() {
+    item1.innerHTML ='<td></td>'
+    window.alert("Item removed")
+}
+
+const item2 = document.querySelector("#item2")
+function rv2() {
+    item2.innerHTML ='<td></td>'
+    window.alert("Item removed")
+}
+
+const item3 = document.querySelector("#item3")
+function rv3() {
+    item3.innerHTML ='<td></td>'
+    window.alert("Item removed")
+}
+
+const item4 = document.querySelector("#item4")
+function rv4() {
+    item4.innerHTML ='<td></td>'
+    window.alert("Item removed")
+}
+
+function pay() {
+    window.alert("Order Placed")
+}
+
+const cup = document.querySelector("#cup")
+function apply() {
+    a = cup.value
+    window.alert(a + "\n" +"Coupon applied")
+}
